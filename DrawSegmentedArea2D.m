65c54add9a40be08e27cf9852b1a13e594292a94
jxchen01 <jchen16@nd.edu>
2015-01-02 15:40:46 -0500

    "enable multiple contours; add repelling force; improve visualization functions"

---
+++ DrawSegmentedArea2D.m
@@ -0,0 +1,28 @@
+function J=DrawSegmentedArea2D(Ps,I)
+% Draw the contour
+% (1) as regions in a logical image, 
+% (2) as curves in the raw image (only display no return)
+%
+%  J=DrawSegmentedArea2D(P,I)
+%
+% inputs,
+%  Ps : The conotours, N cells of size Ni x 2
+%  I : The raw image
+%
+% outputs,
+%  J : The binary image with all regions enclosed by contours
+%
+% Function is written by Jianxu Chen (University of Notre Dame) on Jan 2015
+[xdim,ydim]=size(I);
+J=false(xdim,ydim);
+
+for i=1:1:numel(Ps)
+    J(Ps{i}.region)=true;
+    P=Ps{i}.pts;
+    se=strel('disk',Ps{i}.thickness-1,0);
+    tmp=false(xdim,ydim);
+    tmp(round(P(1,1)),round(P(1,2)))=true;
+    tmp(round(P(end,1)),round(P(end,2)))=true;
+    J = J | imdilate(tmp,se);
+end
+imshow(J)
